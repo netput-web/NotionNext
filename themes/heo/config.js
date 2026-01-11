19a89342045ba67d74a53af75736703c0f61b0ed
蜗牛老师 <netput@qq.com>
2026-01-12 02:12:31 +0800

    Update titles for site statistics in config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -123,10 +123,10 @@ const CONFIG = {
   HEO_SOCIAL_CARD_URL: 'https://blog.kk218.com/shequn',
 
   // 底部统计面板文案
-  HEO_POST_COUNT_TITLE: '文章总数',
-  HEO_SITE_TIME_TITLE: '建站天数',
-  HEO_SITE_VISIT_TITLE: '访问数量',
-  HEO_SITE_VISITOR_TITLE: '访客总数',
+  HEO_POST_COUNT_TITLE: '文章数:',
+  HEO_SITE_TIME_TITLE: '建站天数:',
+  HEO_SITE_VISIT_TITLE: '访问量:',
+  HEO_SITE_VISITOR_TITLE: '访客数:',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
